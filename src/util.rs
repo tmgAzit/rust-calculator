75c13c93fc6859e8711ef9ac2ed2bdebf78d2493
ajit <azit.ghising@gmail.com>
2026-05-20 16:14:32 +0930

    Defining raise macro as a designator patterns with Error handling syntax and indicating how the arguments are accepted for zero or more times.

---
+++ src/util.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! raise {
+    ($x: expr) => {
+        {
+            ::std::result::Result::Err($x)?;
+            panic!("unreachable code");
+        }
+    };
+    ($x: expr, $msg:expr) => {
+        {
+            let msg = $msg.into();
+            raise!($x(msg));
+        }
+    };
+    ($x: expr, $format:expr, $($arg:expr),*) => {
+        raise!($x, format!($format, $($arg), *))
+    };
+}
